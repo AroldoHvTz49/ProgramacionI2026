28ca583ed92de9e4955cb3a6008c1aff535c79fb
Aroldo Najera <arowalnaca@gmail.com>
2026-02-20 20:23:13 -0600

    videos

---
+++ videos/Aplicacion1/Aplicacion1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Video 3
+Console.WriteLine("Video 3");
+Console.WriteLine(87);
+Console.WriteLine("Informacion");
+Console.WriteLine(7 + 10);
+
+//Video 4
+Console.WriteLine("Video 4");
+int entero = 87;
+double decima = 2.14;
+bool booleano = true; // true o false
+char caracter = 'a'; // solo 2 caracteres, se usan comillas simples
+float decimales32bit = 1.50f; // se le agrega la letra f al final para indicar que es un float
+string cadena = "Informacion"; // se usan comillas dobles para las cadenas de texto
